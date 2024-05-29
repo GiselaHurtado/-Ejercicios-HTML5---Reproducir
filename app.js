3a6747b5f2ff62485b4cd700fd6a057dad2b2185
“Gisela-Soledad-Hurtado” <hurtadogiselasoledad@gmail.com>
2024-05-29 23:27:20 +0200

    added JavaScript

---
+++ app.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const emojis = document.querySelectorAll('.emoji');
+    emojis.forEach(emoji => {
+      emoji.classList.add('emoji');
+    });
+  });
